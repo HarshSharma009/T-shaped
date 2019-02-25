2fea0ad47d83773cad97da005b4c97537da6fd5f
HarshSharma009 <47885066+HarshSharma009@users.noreply.github.com>
2019-02-25 20:56:41 +0530

    Update Login.php

--- Login.php
+++ Login.php
@@ -8,7 +8,7 @@ include("config.php");
 
 <head>
   <meta charset="UTF-8">
-  <title>Dark blue Login Page</title>
+  <title>Login Page</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   
   <link rel='stylesheet' href='https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/css/bootstrap.min.css'>
